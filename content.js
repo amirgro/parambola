c942bc1c54b6b373b334c0133578e4f04da02a3e
amirgro <groisman16@gmail.com>
2020-07-21 20:41:28 +0300

    rename

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.message === 'TabUpdated') {
+  if (request.message === 'TabUpdated') {debugger;
     chrome.runtime.sendMessage({newIcon: document.location.href.includes('obAbtest') ? 'fms.png' : 'smartfeed.png'});
   }
 })
